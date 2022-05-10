a5d25185ad9d1a1ed177ecdef486f28ecfdeea43
Hector <hector141599@gmail.com>
2022-05-10 16:14:57 -0500

    Revisando la documentación y la función de Trello.prototype.addCard

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ var trello= new Trello("key","token");
 var cardTitle= `Card Nueva ${new Date()}`;
 
 console.log("Ejecutando");
+console.log(Trello.prototype.addCard);
 
 trello.addCard(cardTitle,"LaunchX Card Description", "IdList",
     function(error,trelloCard){
@@ -13,3 +14,5 @@ trello.addCard(cardTitle,"LaunchX Card Description", "IdList",
             console.log('Added card: ',trelloCard);
         }
     });
+// La función Trello.prototype.addCard
+// Esta nos permite crear cards para tableros en trello
